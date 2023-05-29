62ba39c30be2f14a3160952091256cc1fac29fb4
Muhammad Rivandra <rivandra0@gmail.com>
2023-05-29 18:40:25 +0700

    refactoring user seeds

--- src/seeds/UserSeed.js
+++ src/seeds/UserSeed.js
@@ -4,8 +4,8 @@ const prisma = new PrismaClient()
 
 
 const hashedPassword = async () => await bcrypt.hash('muaracoder', 10)
-const verifiedUserData = { email: 'rivandra0@gmail.com', password:await hashedPassword(), isVerified:true } 
-const unverifiedUserData = { email: 'tuyulmohak0@gmail.com', password:await hashedPassword() }
+const verifiedUserData = { email: 'loginverified0@mail.com', password:await hashedPassword(), isVerified:true } 
+const unverifiedUserData = { email: 'loginunverified@mail.com', password:await hashedPassword() }
 const successVerifyTestUser = { email: 'verifysuccess@mail.com', password:await hashedPassword(), lastSentEmail: new Date("2000-01-17T16:45:30")  } //this is absolutely more than 1hrs
 const failVerifyTestUser = { email: 'verifyfail@mail.com', password:await hashedPassword() }
 
